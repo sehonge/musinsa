6b7ef9b5f27401bb40406899b9ada3a37eaad762
sehonge <sehong1602@naver.com>
2025-05-19 23:09:55 +0900

    merge conflict resolve

---
+++ src/main/kotlin/kr/musinsa/MusinsaApplication.kt
@@ -0,0 +1,11 @@
+package kr.musinsa
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MusinsaApplication
+
+fun main(args: Array<String>) {
+    runApplication<MusinsaApplication>(*args)
+}
